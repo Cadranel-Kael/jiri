029c4e3603f630d5cc78db991942eb3faf7557bb
kael-cadranel <kael.cadranel00@gmail.com>
2023-10-31 15:29:10 +0100

    added redirection to ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\URL;
+
 class ContactController extends Controller
 {
     public function index()
     {
         return view('contacts.index');
+    }
+
+    public function create()
+    {
+        return Redirect::to(URL::route('contacts.index', ['#create']));
+    }
+
+    public function edit()
+    {
+        return Redirect::to(URL::route('contacts.index', ['#edit']));
     }
 }
--- resources/views/components/modal-form.blade.php
+++ resources/views/components/modal-form.blade.php
@@ -4,10 +4,10 @@
     x-show="show"
     x-init="show = (location.hash === '#{{ $name }}')"
     @hashchange.window = "show = (location.hash === '#{{ $name }}')"
-    @open-modal.window="($event.detail.name === {{ $name }}) ? (show = true, location.hash = '#{{ $name }}') : '';"
+    style="display: none"
 >
     <a class="fixed block h-screen cursor-default w-screen bg-black opacity-75 top-0 left-0 z-10" href="#"></a>
-    <form action="{{ $action ?? '' }}" method="{{ $method ?? '' }}" class="fixed z-20 w-fit h-fit absolute bg-white rounded flex flex-col p-4 inset-0 m-auto">
+    <form action="{{ $action ?? '' }}" method="{{ $method ?? '' }}" class="items-stretch fixed z-20 w-fit h-fit absolute bg-white rounded flex flex-col p-4 inset-0 m-auto">
         @csrf
         <div class="flex justify-between">
             {{ $title }}
