1aed80d211aa483c5e430156858894cf4e18f445
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-03 14:20:04 +0100

    Updated route

---
+++ resources/views/components/project-card.blade.php
@@ -0,0 +1,6 @@
+<div
+    class="bg-white drop-shadow flex flex-col min-w-col-2-sm items-center align-center py-3 px-2 rounded gap-2 place-content-between">
+    <span class="text-center text-ellipsis font-bold">{{ $title }}</span>
+    <span class="text-center h-12 overflow-hidden text-ellipsis">{{ $description }}</span>
+    <x-link-outline href="" value="Voire le projet"/>
+</div>
