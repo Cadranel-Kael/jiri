a342193aaacfedad4b79762d6f22256d0c67ba1d
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-03 14:19:48 +0100

    Added relationship project user many to one

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Contact::class);
     }
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
