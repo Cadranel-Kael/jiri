946cdc67daa215510c034013c208cffdd3e326ab
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-07 08:18:48 +0100

    Edited dashboard to be adaptive for screen sizes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,20 @@
         {{ 'Dashboard' }}
     </x-slot>
     <h2 class="font-bold lg:text-h2 text-h2-sm px-4 mb-2 lg:mb-5">Épreuves à venir</h2>
-    <div class="flex flex-row gap-4 overflow-scroll">
+    <div class="flex flex-row gap-4 p-4 overflow-scroll mb-10">
         @foreach($events as $event)
-            <x-event-card name="{{ $event->name }}" date="{{ $event->date }}" class="items-stretch flex gap-5 overflow-x-auto px-4 pb-4 mb-4 lg:mb-20"/>
+            <x-event-card name="{{ $event->name }}" date="{{ $event->date }}"
+                          class="items-stretch flex gap-5 overflow-x-auto px-4 pb-4 mb-4 lg:mb-20"/>
         @endforeach
     </div>
-    <h2 class="font-bold lg:text-h2 text-h2-sm mx-4 mb-2 lg:mb-5">Résultats des dernières années</h2>
-    <h2 class="font-bold lg:text-h2 text-h2-sm mx-4 mb-2 lg:mb-5">Épreuves</h2>
-    <livewire:events-stats class="bg-white rounded mx-4 p-4"/>
+    <div class="flex flex-col lg:flex-row lg:mr-32">
+        <div class="flex-1">
+        <h2 class="font-bold lg:text-h2 text-h2-sm mx-4 mb-2 lg:mb-5">Résultats des dernières années</h2>
+        </div>
+        <div class="flex-1">
+            <h2 class="font-bold lg:text-h2 text-h2-sm mx-4 mb-2 lg:mb-5">Épreuves</h2>
+            <livewire:events-stats class="bg-white rounded mx-4 p-4"/>
+        </div>
+    </div>
+
 </x-app-layout>
