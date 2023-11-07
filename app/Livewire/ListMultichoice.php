e90e5eca029b27912bab95373d727c0b8e33d7de
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-07 08:20:38 +0100

    created multi choice list component

---
+++ app/Livewire/ListMultichoice.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ListMultichoice extends Component
+{
+
+
+    public function render()
+    {
+        return view('livewire.list-multichoice');
+    }
+}
---
+++ resources/views/livewire/list-multichoice.blade.php
@@ -0,0 +1,12 @@
+<div class="rounded drop-shadow bg-white">
+    <div class="flex">
+        <div>Trier par</div>
+        <x-search search="search"/>
+    </div>
+    <div>
+        <div class="flex justify-between rounded bg-white drop-shadow">
+            <div>CV</div>
+            <button>Add</button>
+        </div>
+    </div>
+</div>
