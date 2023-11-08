86d7f444cba9f61502881455bdf92a23f9520f27
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-08 10:52:41 +0100

    moved sorts

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -2,11 +2,5 @@
     <x-slot name="heading">
         {{ __('contacts.title') }}
     </x-slot>
-    <label for="sort"></label>
-    <select name="sort" id="sort" wire:model.live="sort">
-        <option id="name" value="name">Nom</option>
-        <option id="email" value="email">Email</option>
-        <option id="created" value="created_at">Ajoutée</option>
-    </select>
-    <livewire:contacts-list/>
+    <livewire:contact-livewire-controller/>
 </x-app-layout>
---
+++ resources/views/livewire/contacts.blade.php
@@ -0,0 +1,42 @@
+<div>
+    <x-modal-form submit="save" name="create">
+        <x-slot:title>
+            <h2 class="text-h3 mb-12 font-bold">{{ __('contacts.add_new') }}</h2>
+        </x-slot:title>
+        <x-slot:body>
+            <x-input-image class="mb-10 align-center"></x-input-image>
+            <x-input class="mb-10" label="{{ __('form.full_name') }}" name="name" placeholder="Wilson Jenny"></x-input>
+            <x-input class="mb-10" label="{{ __('form.email') }}" name="email" placeholder="jenny.wilson@mail.com"></x-input>
+        </x-slot:body>
+        <x-slot:footer>
+            <div class="flex justify-center">
+                <x-button-primary type="submit">{{ __('contacts.add_new') }}</x-button-primary>
+            </div>
+        </x-slot:footer>
+    </x-modal-form>
+    <div class="flex flex-col lg:flex-row justify-between mb-8">
+        <div class="flex flex-col gap-4 mr-4 mb-5 lg:flex-row">
+            <x-link-primary class="w-full lg:w-fit" href="#create">{{ __('contacts.add_new') }}</x-link-primary>
+            <x-link-white class="w-full lg:w-fit" href="#import">{{ __('contacts.import') }}</x-link-white>
+        </div>
+        <div class="flex flex-col gap-4 mr-4 mb-5 lg:flex-row">
+            <x-sort sort="sort" :options="$this->sortable_by"/>
+            <x-search class="w-full lg:w-fit" search="contact"/>
+        </div>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-6 gap-4 mr-4">
+        @foreach($this->contacts as $contact)
+            <x-profile-183 livewire:revenue lazy="on-load" src="{{ $contact->image_url }}" email="{{ $contact->email }}"
+                           name="{{ $contact->name }}"/>
+        @endforeach
+    </div>
+    @if($this->contacts->isEmpty())
+        <div>
+            <span>{{ __('form.no_results') }}</span>
+            <x-link-primary href="#create">{{ __('contacts.add_new') }}</x-link-primary>
+        </div>
+    @else
+        <button wire:click="load_more">Load more</button>
+    @endif
+
+</div>
