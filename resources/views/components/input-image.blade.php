f4bf84738fc5a7890d5d4d0c3e29215159b2e851
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-08 10:53:23 +0100

    added input image

---
+++ resources/views/components/input-image.blade.php
@@ -0,0 +1,7 @@
+<div {{ $attributes(['class'=>'flex']) }}>
+    <label for="image" class="sr-only">Votre image</label>
+    <div class="w-32 h-32 bg-black-50 rounded-full mr-12">
+        <img src="" alt="" width="">
+    </div>
+    <input class="border-dashed border-black border-2 rounded p-2" type="file" name="image" id="image" accept="image/*">
+</div>
