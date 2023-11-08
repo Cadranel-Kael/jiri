7975176ba66770a821d5c37ce2273b588ffeafb6
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-08 10:53:59 +0100

    added transition

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
-<a title="{{ $name }}" href="{{ $href }}" class="rounded flex items-center group hover:bg-white {{ $active ? 'bg-white' : 'hover:bg-white' }}">
-    <div class="group rounded p-2 {{ $active ? 'bg-white' : 'group-hover:bg-white' }} h-10 w-10 flex">
+<a title="{{ $name }}" href="{{ $href }}" class="rounded flex items-center group hover:bg-white transition duration-150 ease-out {{ $active ? 'bg-white' : 'hover:bg-white' }}">
+    <div class="group rounded p-2 transition duration-150 ease-out {{ $active ? 'bg-white' : 'group-hover:bg-white' }} h-10 w-10 flex">
         <svg role="img"
-             class="w-6 h-auto fill-white {{ $active ? 'fill-primary' : 'group-hover:fill-primary'}}" width="{{ $width }}"
+             class="w-6 h-auto transition duration-150 ease-out {{ $active ? 'fill-primary' : 'group-hover:fill-primary fill-white'}}" width="{{ $width }}"
              height="{{ $height }}">
             <use xlink:href="{{ asset('icons/icons.svg#' . $icon) }}"/>
         </svg>
@@ -10,5 +10,5 @@
         x-show="expanded"
         x-transition
         style="display: none"
-        class="ml-6 {{ $active ? 'text-primary' : 'group-hover:text-primary' }}">{{ $name }}</span>
+        class="ml-6 transition duration-150 ease-out {{ $active ? 'text-primary' : 'group-hover:text-primary' }}">{{ $name }}</span>
 </a>
