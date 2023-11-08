ca3a5877dfb3dd476dd2b973ac930bc9ec7a58bb
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-08 10:53:38 +0100

    added transition

---
+++ resources/views/components/link-white.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $href }}" {{ $attributes(['class' => 'transition duration-150 ease-out block text-center p-2.5 drop-shadow rounded block-inline cursor-pointer bg-white text-black hover:text-white hover:bg-black']) }}>
+    {{ $slot }}
+</a>
+
