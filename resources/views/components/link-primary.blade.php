a9705f3f3bdbb3be632a959c627feb0496429c84
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-08 10:53:33 +0100

    added transition

--- resources/views/components/link-primary.blade.php
+++ resources/views/components/link-primary.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ $href }}" {{ $attributes(['class' => 'block text-center p-2.5 drop-shadow rounded block-inline cursor-pointer bg-primary text-white hover:bg-primary-75']) }}>
+<a href="{{ $href }}" {{ $attributes(['class' => 'transition duration-150 ease-out block text-center p-2.5 drop-shadow rounded block-inline cursor-pointer bg-primary text-white hover:bg-white hover:text-primary']) }}>
     {{ $slot }}
 </a>
