efc5323298771d36159b9384801cfa9dde4bc347
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-10 14:26:46 +0100

    renamed events-list -> events

---
+++ resources/views/livewire/events.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <x-top-bar
+        :create-href="route('events.create')"
+        :create-label="__('events.create')"
+        import-href="#"
+        :import-label="__('events.import')"
+        sort="sort"
+        order="order"
+        :options="$this->sortables"
+        search="search"
+    />
+    <div class="grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-6 gap-4 mx-4">
+        @foreach($this->events as $event)
+            <x-event-card name="{{ $event->name }}" date="{{ $event->date }}"/>
+        @endforeach
+    </div>
+</div>
