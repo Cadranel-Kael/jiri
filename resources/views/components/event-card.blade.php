2e376445abb2b3e3acd17fdbbf4bb25dee3aa67f
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-10 14:25:26 +0100

    refactored with new components

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -1,11 +1,7 @@
 <div
     class="bg-white drop-shadow flex flex-col min-w-col-2-sm items-center align-center py-3 px-2 rounded gap-2 place-content-between">
-    @if($date > \Carbon\Carbon::today())
-        <span class="py-1 px-6 bg-primary text-white rounded">Future</span>
-    @else
-        <span class="py-1 px-6 bg-red-600 text-white rounded">Passée</span>
-    @endif
-    <span class="text-center h-12 text-ellipsis">{{ $name }}</span>
-    <span class="text-black-50">{{ Carbon\Carbon::parse($date)->translatedFormat('d M Y') }}</span>
+    <x-date-pill :date="$date"/>
+    <span class="text-center h-12 text-lg text-ellipsis">{{ $name }}</span>
+    <x-date class="text-black-50" :date="$date" />
     <x-link-outline href="" value="Voire l'épreuve"/>
 </div>
