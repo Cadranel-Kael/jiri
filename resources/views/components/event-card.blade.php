e5193fe1a45ce46bd42c147d0b8d62fb210006fd
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-11 21:43:17 +0100

    updated component so that you only need to send an event collection

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -1,7 +1,7 @@
 <div
     class="bg-white drop-shadow flex flex-col min-w-col-2-sm items-center align-center py-3 px-2 rounded gap-2 place-content-between">
-    <x-date-pill :date="$date"/>
-    <span class="text-center h-12 text-lg text-ellipsis">{{ $name }}</span>
-    <x-date class="text-black-50" :date="$date" />
-    <x-link-outline :href="route('events.show', $id)" value="Voire l'épreuve"/>
+    <x-date-pill :date="$event->date"/>
+    <span class="text-center h-12 text-lg text-ellipsis">{{ $event->name }}</span>
+    <x-date class="text-black-50" :date="$event->date" />
+    <x-link-outline :href="route('events.show', $event->id)" value="Voire l'épreuve"/>
 </div>
