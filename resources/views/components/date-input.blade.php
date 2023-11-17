bc29cd3aae470f09cf3c1422d163bac443ac177b
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-17 15:49:23 +0100

    added $attributes and error message

--- resources/views/components/date-input.blade.php
+++ resources/views/components/date-input.blade.php
@@ -1,4 +1,7 @@
-<div>
-    <label for="{{ $name }}">{{ $label }}</label>
-    <input class="rounded border-none drop-shadow" type="date" name="{{ $name }}" id="{{ $name }}">
+<div {{ $attributes(['class'=>'flex flex-col']) }}>
+    <label class="font-bold" for="{{ $name }}">{{ $label }}</label>
+    <input class="rounded border-none drop-shadow" wire:model="{{ $name }}" type="date" name="{{ $name }}" id="{{ $name }}">
+    @error($name)
+    {{ $message }}
+    @enderror
 </div>
