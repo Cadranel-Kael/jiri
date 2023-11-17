e3426a6da26a2292f38b4bd538f0209c75a0e28e
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-17 15:50:11 +0100

    removed unnecessary @csrf

--- resources/views/components/modal-form.blade.php
+++ resources/views/components/modal-form.blade.php
@@ -6,7 +6,6 @@
 >
     <a class="fixed block h-screen cursor-default w-screen bg-black opacity-75 top-0 left-0 z-50" aria-hidden="true" href="#"></a>
     <form wire:submit="{{ $submit }}" class="items-stretch fixed z-50 w-fit h-fit absolute bg-white rounded flex flex-col p-4 inset-0 m-auto">
-        @csrf
         <div class="flex relative">
             {{ $title }}
             <a href="#" class="absolute right-0">
