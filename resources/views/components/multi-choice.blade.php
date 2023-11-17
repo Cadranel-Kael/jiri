62e14be0239c3805d2b1c5b4b11c50c565077425
kael-cadranel <kael.cadranel00@gmail.com>
2023-11-17 15:50:37 +0100

    [STYLE] fixed styling if multi choice list

--- resources/views/components/multi-choice.blade.php
+++ resources/views/components/multi-choice.blade.php
@@ -1,9 +1,9 @@
-<div>
+<div class="rounded drop-shadow bg-white p-2">
     <div class="flex flex-wrap flex-stretch w-full">
-        <x-sort :sort="$sort" :order="$order" :options="$sortables"/>
-        <x-search class="grow" search="search"/>
+        <x-sort class="mr-3 mb-2" :change-order="$changeOrder" :sort="$sort" :order="$order" :options="$sortables"/>
+        <x-search class="grow" :search="$search"/>
     </div>
-    <ul>
+    <ul class="flex pt-2 justify-between pb-2 h-96 overflow-y-auto flex-col rounded bg-white gap-2">
         {{ $addedList }}
         {{ $list }}
     </ul>
