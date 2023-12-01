20bbfbc9aac9f1fdce47dacdf37a33bec17ac11f
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-01 09:11:40 +0100

    [STYLE] all cards are of the same size now

--- resources/views/components/evaluator.blade.php
+++ resources/views/components/evaluator.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white justify-between drop-shadow p-4 flex flex-col items-center box-border rounded">
+<div class="w-48 bg-white justify-between drop-shadow p-4 flex flex-col items-center box-border rounded">
     <img class="rounded-full object-cover w-24 h-24 mb-6" src="{{ $evaluator->src }}" alt="" width="106" height="106"
          loading="lazy">
     <div class="text-ellipsis overflow-hidden w-full text-center mb-2">{{ $evaluator->name }}</div>
