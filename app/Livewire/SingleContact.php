4f05b9121ba4cde40e13b969703f1e4540f99ff8
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-01 09:15:31 +0100

    [FUNCTIONALITY] can edit contact from single contact

--- app/Livewire/SingleContact.php
+++ app/Livewire/SingleContact.php
@@ -2,18 +2,33 @@
 
 namespace App\Livewire;
 
+use App\Models\Contact;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class SingleContact extends Component
 {
     public int $id;
+    public $name, $email;
 
     #[Computed]
     public function contact()
     {
         return auth()->user()->contacts()->where('id', $this->id)->first();
+    }
+
+    #[Computed]
+    public function name()
+    {
+        $this->name = $this->contact()->name;
+    }
+
+    #[Computed]
+    public function email()
+    {
+        $this->email = $this->contact()->email;
     }
 
     #[Computed]
@@ -25,24 +40,52 @@ class SingleContact extends Component
     public function getProjectAverage($event_id, $project_id, $total = 100)
     {
         return round($this
-            ->events
-            ->where('id', $event_id)
-            ->first()
-            ->presentations()
-            ->where('project_id', $project_id)
-            ->first()
-            ->scores
-            ->pluck('score')
-            ->avg()/100*$total);
+                ->events
+                ->where('id', $event_id)
+                ->first()
+                ->presentations()
+                ->where('project_id', $project_id)
+                ->first()
+                ->scores
+                ->pluck('score')
+                ->avg() / 100 * $total);
     }
 
     public function getAverage($event_id, $total = 100)
     {
         foreach ($this->events->where('id', $event_id)->first()->projects as $project) {
-            $scores[] = $this->getProjectAverage($event_id, $project->id)*$project->pivot->weight;
+            $scores[] = $this->getProjectAverage($event_id, $project->id) * $project->pivot->weight;
         }
 
-        return round(array_sum($scores)/count($scores)/100*$total);
+        return round(array_sum($scores) / count($scores) / 100 * $total);
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5',
+            'email' => 'required|email',
+        ];
+    }
+
+    public function validationAttributes()
+    {
+        return [
+            'name' => __('form.full_name'),
+            'email' => __('form.email'),
+        ];
+    }
+
+    public function update()
+    {
+        $this->validate();
+
+        auth()->user()->contacts()->where('id', $this->id)->update([
+            'name' => $this->name,
+            'email' => $this->email,
+        ]);
+
+        return Redirect::to(route('contacts.show', ['contact' => $this->contact->id]));
     }
 
     public function render()
