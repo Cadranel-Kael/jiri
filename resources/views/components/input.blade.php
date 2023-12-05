c6f4a74df65f50c9db6968e0de0a910e0f55e2c8
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-05 12:26:37 +0100

    [REFACTORING] [BUG] error now is taken from model name if it exists

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -6,9 +6,9 @@
             <span class="sr-only">{{ __('form.obligatory') }}</span>
         @endisset
     </label>
-    <input value="{{ $value ?? '' }}" wire:model="{{ $model ?? $name }}" class="border-b-2 border-0" placeholder="{{ $placeholder ?? $label }}" type="text" name="{{ $name }}"
+    <input value="{{ $value ?? '' }}" wire:model.live="{{ $model ?? $name }}" class="border-b-2 border-0" placeholder="{{ $placeholder ?? $label }}" type="text" name="{{ $name }}"
            id="{{ $name }}">
-    @error($name)
+    @error($model ?? $name)
     {{ $message }}
     @enderror
 </div>
