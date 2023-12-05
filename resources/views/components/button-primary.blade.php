4ff556b6cb38bb278603a9de7eee233fd8da8bee
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-05 12:20:27 +0100

    [BUG] fixed attributes

--- resources/views/components/button-primary.blade.php
+++ resources/views/components/button-primary.blade.php
@@ -1,3 +1,8 @@
-<button type="{{ $type }}" {{ $attributes(['class'=>'p-2.5 drop-shadow rounded block bg-primary text-white hover:bg-primary-75']) }}>
+@props([
+    'type' => 'button'
+    ])
+
+<button
+    type="{{ $type }}" {{ $attributes->merge(['class'=>'p-2.5 drop-shadow rounded block bg-primary text-white hover:bg-primary-75']) }}>
     {{ $slot }}
 </button>
