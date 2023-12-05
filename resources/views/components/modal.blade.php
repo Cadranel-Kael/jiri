d833751802b077f9ac9cacb70a7a1f1292354427
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-05 12:27:39 +0100

    [COMPONENT] created general modal component

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,77 +1,37 @@
 @props([
     'name',
-    'show' => false,
-    'maxWidth' => '2xl'
+    'title' => null,
+    'footer' => null,
 ])
 
-@php
-$maxWidth = [
-    'sm' => 'sm:max-w-sm',
-    'md' => 'sm:max-w-md',
-    'lg' => 'sm:max-w-lg',
-    'xl' => 'sm:max-w-xl',
-    '2xl' => 'sm:max-w-2xl',
-][$maxWidth];
-@endphp
-
 <div
-    x-data="{
-        show: @js($show),
-        focusables() {
-            // All focusable element types...
-            let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
-            return [...$el.querySelectorAll(selector)]
-                // All non-disabled elements...
-                .filter(el => ! el.hasAttribute('disabled'))
-        },
-        firstFocusable() { return this.focusables()[0] },
-        lastFocusable() { return this.focusables().slice(-1)[0] },
-        nextFocusable() { return this.focusables()[this.nextFocusableIndex()] || this.firstFocusable() },
-        prevFocusable() { return this.focusables()[this.prevFocusableIndex()] || this.lastFocusable() },
-        nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % (this.focusables().length + 1) },
-        prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement)) -1 },
-    }"
-    x-init="$watch('show', value => {
-        if (value) {
-            document.body.classList.add('overflow-y-hidden');
-            {{ $attributes->has('focusable') ? 'setTimeout(() => firstFocusable().focus(), 100)' : '' }}
-        } else {
-            document.body.classList.remove('overflow-y-hidden');
-        }
-    })"
-    x-on:open-modal.window="$event.detail == '{{ $name }}' ? show = true : null"
-    x-on:close.stop="show = false"
-    x-on:keydown.escape.window="show = false"
-    x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
-    x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
+    x-data="{ show: false, name: '{{ $name }}' }"
     x-show="show"
-    class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
-    style="display: {{ $show ? 'block' : 'none' }};"
+    x-on:open-modal.window="show = ($event.detail.name === name)"
+    x-on:close-modal.window="show = ($event.detail.name === name)"
+    x-on:keydown.escape.window="show = false"
+    x-on:form-submitted.window="show = false"
+    class="fixed inset-0 z-50 flex items-center justify-center"
+    style="display: none;"
 >
-    <div
-        x-show="show"
-        class="fixed inset-0 transform transition-all"
-        x-on:click="show = false"
-        x-transition:enter="ease-out duration-300"
-        x-transition:enter-start="opacity-0"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0"
-    >
-        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-    </div>
-
-    <div
-        x-show="show"
-        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
-        x-transition:enter="ease-out duration-300"
-        x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-    >
-        {{ $slot }}
+    <button x-data x-on:click="show = false" class="fixed block h-screen cursor-default w-screen bg-black opacity-75 top-0 left-0 z-50" aria-hidden="true"></button>
+    <div class="items-stretch top-0 px-8 py-6 z-50 w-fit h-fit absolute bg-white rounded flex flex-col p-4 inset-0 m-auto">
+        <div class="flex relative items-center mb-12">
+            {{ $title }}
+            <button x-data x-on:click="show = false" type="button" class="absolute right-0">
+                <svg role="img" class="fill-black w-4 h-auto" width="17.4611" height="19.6782">
+                    <use xlink:href="{{ asset('icons/icons.svg#icon-close') }}"/>
+                </svg>
+                <span class="sr-only">
+                    close
+                </span>
+            </button>
+        </div>
+        <div class="flex flex-col">
+            {{ $body }}
+        </div>
+        <div>
+            {{ $footer }}
+        </div>
     </div>
 </div>
