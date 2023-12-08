9dcbf096ecff51912da5cf18da668a8707bc0120
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-08 17:29:37 +0100

    [UX] added able to add projects

--- app/Livewire/SingleEvent.php
+++ app/Livewire/SingleEvent.php
@@ -2,16 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\EventForm;
 use Illuminate\Database\Eloquent\Builder;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class SingleEvent extends Component
 {
+    public EventForm $form;
 
     public int $id;
-    public bool $showEvaluatorModal = false;
-    public bool $showStudentModal = false;
+
+    public $projectSearch = '';
+    public string $projectSort = 'title';
+    public string $projectOrder = 'ASC';
+    public array $projectSortables = ['title', 'created_at'];
+    public array $addedProjectsIds = [];
+
+    public $editingUrls = [];
+
+    public function mount()
+    {
+        $this->form->setEvent(auth()->user()->events()->where('id', $this->id)->first());
+    }
+
+    public function changeProjectOrder(): void
+    {
+       $this->projectOrder = $this->projectOrder === 'ASC' ? 'DESC' : 'ASC';
+    }
 
     #[Computed]
     public function event()
@@ -19,17 +37,100 @@ class SingleEvent extends Component
         return auth()->user()->events()->where('id', $this->id)->first();
     }
 
+    // --------------
+    // -- Projects --
+    // --------------
+
     #[Computed]
     public function projects()
     {
-        return $this->event->projects;
+        return auth()
+            ->user()
+            ->projects()
+            ->where('title', 'like', '%' . $this->projectSearch . '%')
+            ->whereNotIn('id', $this->event->projects->pluck('id'))
+            ->whereNotIn('id', $this->addedProjectsIds)
+            ->orderBy($this->projectSort, $this->projectOrder)
+            ->paginate(12);
     }
+
+    #[Computed]
+    public function addedProjects()
+    {
+        return auth()
+            ->user()
+            ->projects()
+            ->where('title', 'like', '%' . $this->projectSearch . '%')
+            ->whereIn('id', $this->addedProjectsIds)
+            ->whereNotIn('id', $this->event->projects->pluck('id'))
+            ->orderBy($this->projectSort, $this->projectOrder)
+            ->paginate(12);
+    }
+
+    #[Computed]
+    public function eventProjects()
+    {
+        return $this->event()->projects;
+    }
+
+    public function addProjectId($id)
+    {
+        if (!in_array($id, $this->addedProjectsIds) && auth()->user()->projects()->where('id', $id)->exists()) {
+            $this->addedProjectsIds[] = $id;
+        }
+    }
+
+    public function removeProjectId($id)
+    {
+        if (in_array($id, $this->addedProjectsIds)) {
+            $this->addedProjectsIds = array_diff($this->addedProjectsIds, [$id]);
+        }
+    }
+
+    public function addProjects()
+    {
+        foreach($this->addedProjectsIds as $id) {
+            $this->form->addProject($id, 1);
+        }
+
+        $this->addedProjectsIds = [];
+
+        session()->flash('success', __('events.projects_added'));
+
+        $this->redirect(route('events.show', $this->id));
+    }
+
+    public function removeProject($id)
+    {
+        $this->form->removeProject($id);
+
+        session()->flash('success', __('events.project_removed'));
+
+        $this->redirect(route('events.show', $this->id));
+    }
+
+    // ----------------
+    // -- Evaluators --
+    // ----------------
 
     #[Computed]
     public function evaluators()
     {
         return $this->event->evaluators;
     }
+
+    public function removeEvaluator($id)
+    {
+        $this->form->removeEvaluator($id);
+
+        session()->flash('success', __('events.evaluator_removed'));
+
+        $this->redirect(route('events.show', $this->id));
+    }
+
+    // --------------
+    // -- Students --
+    // --------------
 
     #[Computed]
     public function students()
@@ -41,10 +142,32 @@ class SingleEvent extends Component
         }])->get();
     }
 
-    public function removeProject($id)
+    public function removeStudent($id)
     {
+        $this->form->removeStudent($id);
 
+        session()->flash('success', __('events.student_removed'));
+
+        $this->redirect(route('events.show', $this->id));
     }
+
+    public function toggleEditUrls($presentation_id)
+    {
+        if (isset($this->editingUrls[$presentation_id])) {
+            unset($this->editingUrls[$presentation_id]);
+        } else {
+            $this->editingUrls[$presentation_id] = $this->event->presentations()->where('id', $presentation_id)->first()->urls;
+        }
+    }
+
+    public function updateUrls($presentation_id)
+    {
+        $this->event->presentations()->where('id', $presentation_id)->update(['urls' => $this->editingUrls[$presentation_id]]);
+
+        unset($this->editingUrls[$presentation_id]);
+    }
+
+
 
     public function presentations($contactId)
     {
@@ -62,25 +185,6 @@ class SingleEvent extends Component
         return $matches;
     }
 
-    public function openEvaluatorModal()
-    {
-        $this->showEvaluatorModal = true;
-    }
-
-    public function closeEvaluatorModal()
-    {
-        $this->showEvaluatorModal = false;
-    }
-
-    public function addEvaluator($id)
-    {
-        $this->event->evaluators()->attach($id);
-    }
-
-    public function removeEvaluator($id)
-    {
-        $this->event->evaluators()->detach($id);
-    }
 
     public function render()
     {
--- resources/views/components/student.blade.php
+++ resources/views/components/student.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white justify-between drop-shadow p-4 flex flex-col items-center box-border rounded">
+<div class="bg-white justify-between drop-shadow p-4 m-4 flex flex-col items-center box-border rounded">
     <div class="flex">
         <img class="rounded-full object-cover w-24 h-24 mb-6" src="{{ $student->image_url }}" alt="" width="106"
              height="106"
@@ -10,11 +10,33 @@
     </div>
     <div class="w-full">
         @foreach($student->presentations as $presentation)
-            <div class="flex items-center bg-black-5 p-2.5 rounded flex-col">
+            <div class="flex items-center bg-black-5 p-2.5 rounded flex-col m-4">
                 <div class="flex justify-between">
-                    <div>{{ $presentation->project->title }}</div>
+                    <h3>{{ $presentation->project->title }}</h3>
                 </div>
                 <div>
+                    <h4>{{ __('presentations.links') }}</h4>
+                    <ul>
+                        @foreach($presentation->urls as $index => $url)
+                            <li>
+                                @if(isset($editingUrls[$presentation['id']]))
+                                    <x-input label="url of link" type="text" wire:model="editingUrls.{{ $presentation->id }}.{{ $index }}" />
+                                @else
+                                    <a href="{{ $url }}" target="_blank">{{ $url }}</a>
+                                @endif
+                            </li>
+                        @endforeach
+                    </ul>
+                    @dump(isset($editingUrls[$presentation['id']]))
+                    @if(isset($editingUrls[$presentation['id']]))
+                        <button wire:click="updateUrls({{ $presentation }})">Save</button>
+                        <button wire:click="toggleEditUrls({{ $presentation }})">Cancel</button>
+                    @else
+                        <button wire:click="toggleEditUrls({{ $presentation }})">Edit</button>
+                    @endif
+                </div>
+                <div>
+                    <h4>{{ __('tasks') }}</h4>
                     @foreach($presentation->project->tasks as $task)
                         @php($isMatchingTask = in_array($task, $presentation->tasks))
                         <div class="{{ $isMatchingTask ? 'text-success' : 'text-warning' }}">{{ $task }}</div>
@@ -25,5 +47,3 @@
     </div>
     <x-link-outline value="{{ __('contacts.see_profile') }}" href="{{ route('contacts.show', $student->id) }}"/>
 </div>
-
-
