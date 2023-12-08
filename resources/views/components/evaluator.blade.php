52f35f743454229bfdf0539e2b0d3889a831dfac
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-08 17:23:46 +0100

    [UX] added close

--- resources/views/components/evaluator.blade.php
+++ resources/views/components/evaluator.blade.php
@@ -1,5 +1,10 @@
 <div class="w-48 bg-white justify-between drop-shadow p-4 flex flex-col items-center box-border rounded">
-    <button type="button" wire:key="{{ $evaluator->id }}" wire:click="removeEvaluator({{ $evaluator->id }})">{{ __('events.jury_remove') }}</button>
+    <button class="self-end" wire:key="{{ $evaluator->id }}" wire:click="removeEvaluator({{ $evaluator->id }})">
+        <svg role="img" class="fill-black w-4 h-auto" width="17.4611" height="19.6782">
+            <use xlink:href="{{ asset('icons/icons.svg#icon-close') }}"/>
+        </svg>
+        <span class="sr-only">remove</span>
+    </button>
     <img class="rounded-full object-cover w-24 h-24 mb-6" src="{{ $evaluator->src }}" alt="" width="106" height="106"
          loading="lazy">
     <div class="text-ellipsis overflow-hidden w-full text-center mb-2">{{ $evaluator->name }}</div>
