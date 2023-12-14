172981967ca4e9f392d789389653440fe202feda
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-14 18:16:30 +0100

    [REFACTORING]

--- resources/views/components/text-area.blade.php
+++ resources/views/components/text-area.blade.php
@@ -1,14 +1,24 @@
+@props([
+    'name',
+    'label',
+    'labelSrOnly' => false,
+    'required' => false,
+    'placeholder' => null,
+    'model' => null,
+    'value' => null
+    ])
 <div class="flex flex-col">
     <label for="{{ $name }}" class="font-bold">
         {{ $label }}
-        @isset($required)
-            <span class='text-red'>*</span>
+        @if($required)
+            <span class='text-warning'>*</span>
             <span class="sr-only">{{ __('form.obligatory') }}</span>
-        @endisset
+        @endif
     </label>
-    <textarea wire:model="{{ $model ?? $name }}" class="border-2 border-black rounded" placeholder="{{ $placeholder ?? $label }}" value="{{ $value ?? '' }}" type="text" name="{{ $name }}"
+    <textarea wire:model.live="{{ $model ?? $name }}" class="border-2 border-black rounded"
+              placeholder="{{ $placeholder ?? $label }}" value="{{ $value ?? '' }}" type="text" name="{{ $name }}"
               id="{{ $name }}" rows="3"></textarea>
-    @error($name)
+    @error($model ?? $name)
     {{ $message }}
     @enderror
 </div>
