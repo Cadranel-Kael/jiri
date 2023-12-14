6ce2f2a6793c9920cf7c6a4d99370b1ddabcda2d
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-14 18:08:49 +0100

    [SECURITY] [PROJECTS] confirmation modal and deletion

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -1,19 +1,22 @@
 <div
     class="bg-white drop-shadow flex flex-col min-w-col-2-sm items-center align-center py-3 px-2 rounded gap-2 place-content-between"
+    wire:key="{{ $project->id }}"
 >
     <x-more-options
         :items="[
             [
                 'href' => '?id=#edit',
-                'label' => __('form.edit')
+                'label' => __('form.edit'),
             ],
             [
                 'label' => __('form.delete'),
-                'color' => 'warning'
+                'color' => 'warning',
+                'action' => 'delete(' . $project->id . ')',
+                'confirm' => __('projects.delete'),
             ]
         ]"
     />
     <span class="text-center text-ellipsis font-bold">{{ $project->title }}</span>
     <span class="text-center h-12 overflow-hidden text-ellipsis">{{ $project->description }}</span>
-    <x-link-outline href="" value="Voire le projet"/>
+    <x-link-outline href="{{route('projects.show', $project->id)}}" value="Voire le projet"/>
 </div>
