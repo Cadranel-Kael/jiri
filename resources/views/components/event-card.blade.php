5871f82d15267b10234540f895b0d3359f19e6d1
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-14 17:25:18 +0100

    [COMPONENT] the future/past pill for events now take the status and not the date

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -1,6 +1,6 @@
 <div
     class="bg-white drop-shadow flex flex-col min-w-col-2-sm items-center align-center py-3 px-2 rounded gap-2 place-content-between">
-    <x-date-pill :date="$event->date"/>
+    <x-date-pill :status="$event->status"/>
     <span class="text-center h-12 text-lg text-ellipsis">{{ $event->name }}</span>
     <x-date class="text-black-50" :date="$event->date" />
     <x-link-outline :href="route('events.show', $event->id)" value="Voire l'épreuve"/>
