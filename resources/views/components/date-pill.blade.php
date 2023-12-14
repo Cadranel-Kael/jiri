3984895ae4adb5deabe5f85a19e8c73cd8dad194
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-14 17:25:13 +0100

    [COMPONENT] the future/past pill for events now take the status and not the date

--- resources/views/components/date-pill.blade.php
+++ resources/views/components/date-pill.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @if($date > \Carbon\Carbon::today())
+    @if($status !== 'ended')
         <span class="py-1 px-6 bg-primary text-white rounded">Future</span>
     @else
         <span class="py-1 px-6 bg-red-600 text-white rounded">Passée</span>
