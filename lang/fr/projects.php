585b51a3cd91a925ecc4d9a985a31fb67b505cdf
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-14 18:06:28 +0100

    [LOCALIZATION]

--- lang/fr/projects.php
+++ lang/fr/projects.php
@@ -2,5 +2,10 @@
 
 return [
     'title' => 'Projet',
-    'add_new' => 'Nouveau projet'
+    'add_new' => 'Nouveau projet',
+    'edit' => 'Modifier le projet',
+    'create' => 'Créer un projet',
+
+    'description' => 'Description du projet',
+    'tasks' => 'Tâches du projet',
 ];
