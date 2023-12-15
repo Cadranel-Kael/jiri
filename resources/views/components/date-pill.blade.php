1337082635877ac70062a9be747226b3b9702de4
kael-cadranel <kael.cadranel00@gmail.com>
2023-12-15 11:21:22 +0100

    [UX] [STYLING] future events are now green

--- resources/views/components/date-pill.blade.php
+++ resources/views/components/date-pill.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if($status !== 'ended')
-        <span class="py-1 px-6 bg-primary text-white rounded">Future</span>
+        <span class="py-1 px-6 bg-success text-white rounded">Future</span>
     @else
         <span class="py-1 px-6 bg-red-600 text-white rounded">Passée</span>
     @endif
