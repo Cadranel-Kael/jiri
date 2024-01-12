4bfb5d41ce1f08867b8674569386cecbf00da78f
kael-cadranel <kael.cadranel00@gmail.com>
2024-01-12 18:24:42 +0100

    [REFACTORING] check if weight exists

--- resources/views/components/added-projects.blade.php
+++ resources/views/components/added-projects.blade.php
@@ -3,13 +3,16 @@
     'project',
 
     /** @var string */
-    'remove'
+    'remove',
+
+    'weight' => true,
 ])
 
 <li
         {{ $attributes->class(['flex items-start mx-4 gap-2 drop-shadow items-center bg-black text-white justify-between py-2 px-4 rounded']) }}>
     <div class="flex items-center gap-2.5">
         <span class="font-bold">{{ $project->title }}</span>
+        @if($weight)
         <label for="weight">{{ __('projects.weight') }}</label>
         <input wire:model.live="weight.{{ $project->id }}" class="rounded text-black w-20" placeholder="1" type="number"
                name="weight" id="weight">
