21ad3bfb4200b4c66e6e4149b10c62c4ba4f399b
kael-cadranel <kael.cadranel00@gmail.com>
2024-01-12 18:34:48 +0100

    [STYLE]

--- resources/views/components/item-projects.blade.php
+++ resources/views/components/item-projects.blade.php
@@ -7,7 +7,10 @@
 ])
 
 <li {{ $attributes->class(['flex items-start mx-4 gap-2 drop-shadow items-center bg-white justify-between py-2 px-4 rounded']) }}>
+    <div class="flex-col flex">
     <span class="font-bold">{{ $project->title }}</span>
+    <span class="text-black-50 text-sm">{{ \Carbon\Carbon::parse($project->created_at)->calendar() }}</span>
+    </div>
     <button type="button" wire:key="{{ $project->id }}" wire:click="{{ $add }}">
         <svg role="img" class="w-9 h-auto stroke-black stroke fill-none" width="33"
              height="33">
