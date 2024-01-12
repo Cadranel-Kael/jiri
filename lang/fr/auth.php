9e4d80ad313bc1ec3665525f0c3801607355e6bd
kael-cadranel <kael.cadranel00@gmail.com>
2024-01-12 18:28:14 +0100

    [LOCALIZATION]

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -13,8 +13,10 @@ return [
     |
     */
 
+    'login' => 'Se connecter',
+    'register' => 'S’inscrire',
     'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
     'password' => 'Le mot de passe fourni est incorrect.',
-    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.'
-
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+    'account' => 'Déjà un compte ?',
 ];
